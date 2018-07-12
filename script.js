586d070ceed8c10bf325daf01cf80e3d536be631
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-12 10:35:12 +0200

    Update content for script.js file

--- script.js
+++ script.js
@@ -3,15 +3,21 @@ class Stopwatch extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      running: false,
-      times: {
-        minutes: 0,
-        seconds: 0,
-        miliseconds: 0
-      },
-      list: [],
-      finalList: []
-    };
+        running: false,
+        times: {
+            minutes: 0,
+            seconds: 0,
+            miliseconds: 0
+        },
+        list: [],
+        finalList: []
+      };
+    }
+  static propTypes ={
+      running: React.PropTypes.bool,
+      times: React.PropTypes.number,
+      list: React.PropTypes.array,
+      finalList: React.PropTypes.array
   }
 
   reset = () => {
@@ -26,12 +32,12 @@ class Stopwatch extends React.Component {
 
 
   pad0 = (value) => {
-    let result = value.toString();
-      if (result.length < 2) {
-        result = '0' + result;
-      }
+      let result = value.toString();
+        if (result.length < 2) {
+          result = '0' + result;
+        }
       return result;
-    }
+  }
 
   format = () => {
       let minutes = this.state.times.minutes;
@@ -40,135 +46,181 @@ class Stopwatch extends React.Component {
 
       return `${this.pad0(minutes)}:${this.pad0(seconds)}:${this.pad0(Math.floor(miliseconds))}`;
   }
+
   start = () => {
       if (!this.state.running) {
         this.state.running = true;
         this.watch = setInterval(() => this.step(), 10);
       }
-    }
+  }
 
   step = () => {
-        if (!this.state.running) return;
-        this.calculate();
-      }
+      if (!this.state.running) return;
+      this.calculate();
+  }
 
   calculate = () => {
-    this.setState({
-        times: {
+      this.setState({
+          times: {
                 minutes: this.state.times.minutes,
                 seconds: this.state.times.seconds,
                 miliseconds: this.state.times.miliseconds + 1
-              }
+            }
       });
-
       if (this.state.times.miliseconds >= 100) {
-      this.setState({
-          times: {
-                minutes: this.state.times.minutes,
-                seconds: this.state.times.seconds +1,
-                miliseconds: 0
+          this.setState({
+              times: {
+                    minutes: this.state.times.minutes,
+                    seconds: this.state.times.seconds +1,
+                    miliseconds: 0
                 }
-            });
+          });
       }
       if (this.state.times.seconds >= 60) {
-      this.setState({
-          times: {
-              minutes: this.state.times.minutes + 1,
-              seconds: 0,
-              miliseconds: this.state.times.miliseconds
-              }
+          this.setState({
+              times: {
+                    minutes: this.state.times.minutes + 1,
+                    seconds: 0,
+                    miliseconds: this.state.times.miliseconds
+                }
           });
       }
   }
 
-    stop = () => {
-      this.setState({
-        running: false
-      });
-      clearInterval(this.watch);
-      }
+  stop = () => {
+        this.setState({
+          running: false
+        });
+        clearInterval(this.watch);
+  }
 
-    resettimer = () => {
+  resettimer = () => {
         this.stop();
         this.reset()
-      }
+  }
 
-    addResult = () => {
+  addResult = () => {
+
           let newResult= {
             id: (this.state.list.length),
             result: this.format(),
             myValue: (60000 *this.state.times.minutes) +(1000 * this.state.times.seconds)  + (10 * this.state.times.miliseconds),
           };
-          this.setState({list:[...this.state.list, newResult]});
+          this.setState({list:[...this.state.list, newResult], running: false});
         }
 
-    clearResults = () => {
-      this.setState({
-        list: [],
-        finalList: []
-      });
+  clearResults = () => {
+        this.setState({
+          list: [],
+          finalList: []
+        });
     }
 
 
-    sortResults = () => {
+  sortResults = () => {
         const myList = [...this.state.list];
-        console.log(myList);
-          let sortedList = myList.sort(function(a, b) {return (a.myValue > b.myValue) ? 1 : ((b.myValue > a.myValue) ? -1 : 0);});
+        let sortedList = myList.sort(function(a, b) {return (a.myValue > b.myValue) ? 1 : ((b.myValue > a.myValue) ? -1 : 0);});
         this.setState({
             finalList: sortedList
-          });
+        });
 
-    }
+  }
 
-    deleteResultWithId = (id) => {
-         this.setState({
-             list: this.state.list.filter( ele => ele.id !== id)
-           });
-         }
+  deleteResultWithId = (id) => {
+        this.setState({
+            list: this.state.list.filter( ele => ele.id !== id)
+        });
+  }
 
-    deleteSortedResultWithId = (id) => {
-          this.setState({
-              finalList: this.state.finalList.filter( ele => ele.id !== id)
-            });
-          }
+  deleteSortedResultWithId = (id) => {
+        this.setState({
+            finalList: this.state.finalList.filter( ele => ele.id !== id)
+        });
+  }
 
-    render = () => {
+  render = () => {
         return (
             <div>
-              <div>
-                <nav className={'controls'}>
-                    <h1 className={'controls-title'}>Stopwatch</h1>
-                    <div className={'controls_panel'}>
-                        <a href="#" className= {'button'} id={'start'} onClick = {() => this.start()}>Start</a>
-                        <a href="#" className = {'button'}  id={'stop'} onClick = {() => this.stop()}>Stop</a>
+                <div>
+                    <Controls
+                      start={this.start}
+                      stop={this.stop}
+                      resettimer={this.resettimer}
+                      addResult={this.addResult}
+                      clearResults={this.clearResults}
+                      sortResults={this.sortResults}
+                    />
+                    <Display
+                      time={this.format()}
+                      running={this.state.running}/ >
+
+              </div>
+              <div className={'results-common'}>
+                    <Animation
+                      running={this.state.running} />
+                    <div className={'results-common_list'}>
+                          <h1 className={'table_results'}>Results</h1>
+                          <Results
+                            list={this.state.list}
+                            running={this.state.running}
+                            className = {'results'} deleteResultWithId={this.deleteResultWithId} />
                     </div>
-                    <div className={'controls_panel'}>
-                        <a href="#" className = {'button'} id={'reset'} onClick = {() => this.resettimer()}>Reset</a>
-                        <a href="#" className = {'button'}  id={'save'} onClick = {() => this.addResult()}>Save</a>
+                    <div className={'results-common_list'}>
+                          <h1 className={'table_sorted-results'}>Classification</h1>
+                          <SortedResults
+                            finalList={this.state.finalList}
+                            running={this.state.running}
+                            className = {'sorted-results'}
+                            deleteSortedResultWithId={this.deleteSortedResultWithId} />
                     </div>
-                    <div className={'controls_panel'}>
-                        <a href="#" className = {'button'}  id={'clear'} onClick = {() => this.clearResults()}>Remove</a>
-                        <a href="#" className = {'button'}  id={'none'} onClick={() => this.sortResults()}>Classify</a>
-                    </div>
-                </nav>
-                <Display time={this.format()} running={this.state.running} ></Display>
-            </div>
-            <div className={'results-common'}>
-                <div className={'results-common_list'}>
-                    <h1 className={'table_results'}>Results</h1>
-                    <Results list={this.state.list} className = {'results'} deleteResultWithId={this.deleteResultWithId}>
-                    </Results>
-                </div>
-                <div className={'results-common_list'}>
-                    <h1 className={'table_sorted-results'}>Classification</h1>
-                    <SortedResults finalList={this.state.finalList} className = {'sorted-results'} deleteSortedResultWithId={this.deleteSortedResultWithId}>
-                    </SortedResults>
-                </div>
-            </div>
+              </div>
           </div>
         );
 
       }
+}
+class Controls extends React.Component {
+    constructor(props){
+      super(props)
+    }
+
+    render = () => {
+          return (
+            <nav className={'controls'}>
+                <h1 className={'controls-title'}>Stopwatch</h1>
+                <div className={'controls_panel'}>
+                    <a href="#"
+                      className= {'button'}
+                      id={'start'}
+                      onClick = {() => this.props.start()}>Start</a>
+                    <a href="#"
+                      className = {'button'}
+                      id={'stop'}
+                      onClick = {() => this.props.stop()}>Stop</a>
+                </div>
+                <div className={'controls_panel'}>
+                    <a href="#"
+                      className = {'button'}
+                      id={'reset'}
+                      onClick = {() => this.props.resettimer()}>Reset</a>
+                    <a href="#"
+                      className = {'button'}
+                      id={'save'}
+                      onClick = {() => this.props.addResult()}>Save</a>
+                </div>
+                <div className={'controls_panel'}>
+                    <a href="#"
+                      className = {'button'}
+                      id={'clear'}
+                      onClick = {() => this.props.clearResults()}>Remove</a>
+                    <a href="#"
+                      className = {'button'}
+                      id={'none'}
+                      onClick={() => this.props.sortResults()}>Classify</a>
+                </div>
+            </nav>
+          );
+    }
 }
 
 class Display extends React.Component {
@@ -181,12 +233,17 @@ class Display extends React.Component {
 
     static propTypes ={
       time: React.PropTypes.string.isRequired,
+      running: React.PropTypes.bool.isRequired
     }
 
     render = () => {
-        return(
-        <div className ={`stopwatch ${this.state.running===true?'on-run':'stopped'}`} > {this.props.time}</div>
-      );
+          return(
+            <div className={'stopwatch-common'}>
+              <div className={'decorative'}></div>
+              <div className ={`stopwatch ${this.state.running===true?'on-run':'stopped'}`} > {this.props.time}</div>
+              <div className={'decorative'}></div>
+            </div>
+        );
     }
 }
 
@@ -200,24 +257,25 @@ class Results extends React.Component {
       }
 
     static propTypes = {
-      list: React.PropTypes.array.isRequired
+      list: React.PropTypes.array.isRequired,
+      running: React.PropTypes.bool.isRequired
     }
 
     render () {
         let myresults = this.props.list.map( ele => {
-          return (
-            <li key={ele.id}>
+            return (
+                <li key={ele.id}>
                       Score: <span>{ele.result}</span>
                       <button className = {'button'} onClick = {() => this.props.deleteResultWithId(ele.id)}>x</button>
-            </li>
-          );
+                </li>
+            );
 
-      });
+          });
         return (
               <ul className={'results'}>
                     {myresults}
               </ul>
-      );
+        );
     }
 }
 
@@ -228,29 +286,57 @@ class SortedResults extends React.Component {
           running: false,
           finalList: [],
         };
-      }
+    }
 
     static propTypes = {
-      finalList: React.PropTypes.array.isRequired
+      finalList: React.PropTypes.array.isRequired,
+      running: React.PropTypes.bool.isRequired
     }
 
     render () {
         let sortedResults = this.props.finalList.map( ele => {
-          return (
-            <li key={ele.id}>
+            return (
+                <li key={ele.id}>
                       Mark: <span>{ele.result}</span>
                       <button className = {'button'} onClick = {() => this.props.deleteSortedResultWithId(ele.id)}>X</button>
-            </li>
-          );
+                </li>
+            );
 
-      });
+        });
         return (
               <ul className={'sorted-results'}>
                     {sortedResults}
               </ul>
-      );
+        );
     }
 }
 
+class Animation extends React.Component {
+    constructor(props) {
+      super();
+    }
+    render = ()=> {
+          return (
+            <div className={'results-animation'}>
+                <div className={'results-animation_head'}>
+                </div>
+                <div className={'results-animation_body'}>
+                    <div className={'results-animation_left-arm'}>
+                    </div>
+                    <div className={'results-animation_torso'}>
+                    </div>
+                    <div className={'results-animation_right-arm'}>
+                    </div>
+                </div>
+                <div className={'results-animation_legs'}>
+                    <div className={'results-animation_left-leg'}>
+                    </div>
+                    <div className={'results-animation_right-leg'}>
+                    </div>
+                </div>
+            </div>
+          );
+    }
+}
 var element = React.createElement(Stopwatch);
 ReactDOM.render(element, document.getElementById('app'));
