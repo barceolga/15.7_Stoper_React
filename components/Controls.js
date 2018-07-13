003a048fd0e484c43b970d35b23dff51c9b3a1c7
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-13 13:43:00 +0200

    Update content for Controls.js

--- components/Controls.js
+++ components/Controls.js
@@ -10,14 +10,12 @@ class Controls extends React.Component {
                   <div className={'controls_panel'}>
                       <a href="#" className={'button'} id={'start'} onClick = {() => this.props.start()}>Start</a>
                       <a href="#" className={'button'} id={'stop'} onClick = {() => this.props.stop()}>Stop</a>
-                  </div>
-                  <div className={'controls_panel'}>
+
                       <a href="#" className={'button'} id={'reset'} onClick = {() => this.props.resettimer()}>Reset</a>
                       <a href="#" className={'button'} id={'save'} onClick = {() => this.props.addResult()}>Save</a>
-                  </div>
-                  <div className={'controls_panel'}>
-                      <a href="#" className={'button'} id={'clear'} onClick = {() => this.props.clearResults()}>Remove</a>
-                      <a href="#" className={'button'} id={'sort'} onClick = {() => this.props.sortResults()}>Classify</a>
+
+                      <a href="#" className={'button'} id={'clear'} onClick = {() => this.props.clearResults()}>Clear</a>
+                      <a href="#" className={'button'} id={'sort'} onClick = {() => this.props.sortResults()}>Sort</a>
                   </div>
               </div>
         );
