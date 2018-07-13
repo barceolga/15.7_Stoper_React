650ecbe83e9a3df88acee94d6a7703d4918a6c3a
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-13 12:35:35 +0200

    Add Display.js component

---
+++ components/Display.js
@@ -0,0 +1,20 @@
+class Display extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    static propTypes ={
+      time: React.PropTypes.string.isRequired,
+      running: React.PropTypes.bool.isRequired
+    }
+
+    render = () => {
+          return(
+            <div className={'stopwatch-common'}>
+              <div className={'decorative'}></div>
+              <div className ={`stopwatch ${this.props.running===true?'on-run':'stopped'}`} > {this.props.time}</div>
+              <div className={'decorative'}></div>
+            </div>
+        );
+    }
+}
