1c11aee966e786c7fae653848e34bb63f9712d28
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-14 17:13:37 +0200

    Update content for Display.js file

--- components/Display.js
+++ components/Display.js
@@ -11,9 +11,13 @@ class Display extends React.Component {
     render = () => {
           return(
             <div className={'stopwatch-common'}>
-              <div className={`decorative ${this.props.running===true?'on-run':'stopped'}`}></div>
-              <div className ={`stopwatch ${this.props.running===true?'on-run':'stopped'}`} > {this.props.time}</div>
-              <div className={`decorative ${this.props.running===true?'on-run':'stopped'}`}></div>
+                <div className={'decorative'}>
+                      <i className={`fas fa-clock ${this.props.running===true?'on-run':'stopped'}`}></i>
+                </div>
+                <div className ={`stopwatch ${this.props.running===true?'on-run':'stopped'}`} > {this.props.time}</div>
+                <div className={'decorative-mirror'}>
+                      <i className={`fas fa-clock ${this.props.running===true?'on-run':'stopped'}`}></i>
+                </div>
             </div>
         );
     }
