e546f908c22c77e9738c83bf9431335e1b04d75d
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-22 13:22:10 +0200

    Delete KeyFrames.js file

--- components/KeyFrames.js
+++
@@ -1,9 +0,0 @@
-import styled, { keyframes } from 'styled-components';
-
-  export const movingBall= keyframes `
-        0% {top: Opx; left: 0px}
-        25% {top: -1Opx; left: 0px}
-        50% {top: -2Opx; left: 0px}
-        75% {top: 1Opx; left: 0px}
-        100% {top: Opx; left: 0px}
-    `;
