b2b6343ec917f91cf656c585d49818ea0fc75b04
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-10-17 09:53:59 +0200

    Update style files and Controls.js file (add buttons descriptions)

--- components/Controls.js
+++ components/Controls.js
@@ -8,48 +8,69 @@ class Controls extends React.Component {
               <div className={'controls'}>
                   <h1 className={'controls-title'}>Timer</h1>
                   <div className={'controls_panel'}>
-                      <button
-                        className={'controls-button'}
-                        id={'start'}
-                        onClick = {() => this.props.start()}>
-                          <i className={'fas fa-play'}></i>
-                        </button>
-                      <button
-                        className={'controls-button'}
-                        id={'stop'}
-                        onClick = {() => this.props.stop()}>
-                          <i className={'fas fa-stop'}></i>
-                        </button>
-                      <button
-                        className={'controls-button'}
-                        id={'save'}
-                        onClick = {() => this.props.addResult()}>
-                          <i className={'fas fa-save'}></i>
-                        </button>
-                      <button
-                        className={'controls-button'}
-                        id={'reset'}
-                        onClick = {() => this.props.resettimer()}>
-                          <i className={'fas fa-undo'}></i>
-                        </button>
-                      <button
-                        className={'controls-button'}
-                        id={'clear'}
-                        onClick = {() => this.props.clearResults()}>
-                          <i className={'fas fa-trash'}></i>
-                        </button>
-                      <button
-                        className={'controls-button'}
-                        id={'sort'}
-                        onClick = {() => this.props.sortResults()}>
-                          <i className={'fas fa-sort-numeric-down'}></i>
-                        </button>
+                     <div className={'controls-button_wrap'}>
                         <button
                           className={'controls-button'}
-                          id={'pick'}
-                          onClick = {() => this.props.pickTheBestTime()}>
-                            <i className={'fas fa-trophy'}></i>
+                          id={'start'}
+                          onClick = {() => this.props.start()}>
+                            <i className={'fas fa-play'}></i>
                           </button>
+                          <p className={'controls-button_description'}>Start</p>
+                      </div>
+                      <div className={'controls-button_wrap'}>
+                        <button
+                          className={'controls-button'}
+                          id={'stop'}
+                          onClick = {() => this.props.stop()}>
+                            <i className={'fas fa-stop'}></i>
+                          </button>
+                          <p className={'controls-button_description'}>Stop</p>
+                      </div>
+                      <div className={'controls-button_wrap'}>
+                        <button
+                          className={'controls-button'}
+                          id={'save'}
+                          onClick = {() => this.props.addResult()}>
+                            <i className={'fas fa-save'}></i>
+                          </button>
+                          <p className={'controls-button_description'}>Save</p>
+                      </div>
+                      <div className={'controls-button_wrap'}>
+                        <button
+                          className={'controls-button'}
+                          id={'reset'}
+                          onClick = {() => this.props.resettimer()}>
+                            <i className={'fas fa-undo'}></i>
+                          </button>
+                          <p className={'controls-button_description'}>Reset</p>
+                      </div>
+                      <div className={'controls-button_wrap'}>
+                        <button
+                          className={'controls-button'}
+                          id={'clear'}
+                          onClick = {() => this.props.clearResults()}>
+                            <i className={'fas fa-trash'}></i>
+                          </button>
+                          <p className={'controls-button_description'}>Clear</p>
+                      </div>
+                      <div className={'controls-button_wrap'}>
+                        <button
+                          className={'controls-button'}
+                          id={'sort'}
+                          onClick = {() => this.props.sortResults()}>
+                            <i className={'fas fa-sort-numeric-down'}></i>
+                          </button>
+                          <p className={'controls-button_description'}>Sort</p>
+                        </div>
+                        <div className={'controls-button_wrap'}>
+                          <button
+                            className={'controls-button'}
+                            id={'pick'}
+                            onClick = {() => this.props.pickTheBestTime()}>
+                              <i className={'fas fa-trophy'}></i>
+                            </button>
+                            <p className={'controls-button_description'}>Best</p>
+                          </div>
                   </div>
               </div>
         );
