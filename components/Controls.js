6c6bb36b11fca2f4f1012d370a4e36730741019d
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-10-18 07:34:50 +0200

    Update style files, Controls.js file and add new image file

--- components/Controls.js
+++ components/Controls.js
@@ -7,70 +7,58 @@ class Controls extends React.Component {
         return (
               <div className={'controls'}>
                   <h1 className={'controls-title'}>Timer</h1>
+                  <div className={'controls-description'}>
+                      <p className={'controls-button_description'}>Start</p>
+                      <p className={'controls-button_description'}>Stop</p>
+                      <p className={'controls-button_description'}>Save</p>
+                      <p className={'controls-button_description'}>Reset</p>
+                      <p className={'controls-button_description'}>Clear</p>
+                      <p className={'controls-button_description'}>Sort</p>
+                      <p className={'controls-button_description'}>Best</p>
+                  </div>
                   <div className={'controls_panel'}>
-                     <div className={'controls-button_wrap'}>
                         <button
                           className={'controls-button'}
                           id={'start'}
                           onClick = {() => this.props.start()}>
                             <i className={'fas fa-play'}></i>
                           </button>
-                          <p className={'controls-button_description'}>Start</p>
-                      </div>
-                      <div className={'controls-button_wrap'}>
                         <button
                           className={'controls-button'}
                           id={'stop'}
                           onClick = {() => this.props.stop()}>
                             <i className={'fas fa-stop'}></i>
                           </button>
-                          <p className={'controls-button_description'}>Stop</p>
-                      </div>
-                      <div className={'controls-button_wrap'}>
                         <button
                           className={'controls-button'}
                           id={'save'}
                           onClick = {() => this.props.addResult()}>
                             <i className={'fas fa-save'}></i>
                           </button>
-                          <p className={'controls-button_description'}>Save</p>
-                      </div>
-                      <div className={'controls-button_wrap'}>
                         <button
                           className={'controls-button'}
                           id={'reset'}
                           onClick = {() => this.props.resettimer()}>
                             <i className={'fas fa-undo'}></i>
                           </button>
-                          <p className={'controls-button_description'}>Reset</p>
-                      </div>
-                      <div className={'controls-button_wrap'}>
                         <button
                           className={'controls-button'}
                           id={'clear'}
                           onClick = {() => this.props.clearResults()}>
                             <i className={'fas fa-trash'}></i>
                           </button>
-                          <p className={'controls-button_description'}>Clear</p>
-                      </div>
-                      <div className={'controls-button_wrap'}>
                         <button
                           className={'controls-button'}
                           id={'sort'}
                           onClick = {() => this.props.sortResults()}>
                             <i className={'fas fa-sort-numeric-down'}></i>
                           </button>
-                          <p className={'controls-button_description'}>Sort</p>
-                        </div>
-                        <div className={'controls-button_wrap'}>
                           <button
                             className={'controls-button'}
                             id={'pick'}
                             onClick = {() => this.props.pickTheBestTime()}>
                               <i className={'fas fa-trophy'}></i>
                             </button>
-                            <p className={'controls-button_description'}>Best</p>
-                          </div>
                   </div>
               </div>
         );
